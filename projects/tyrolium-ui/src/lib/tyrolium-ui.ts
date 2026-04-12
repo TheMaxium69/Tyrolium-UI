a92d3031a8634eb3ee7c4a64444db92479bfa8ce
TheMaximeSan <mxm.tournier@gmail.com>
2026-04-12 17:36:15 +0200

    first buttom

--- projects/tyrolium-ui/src/lib/tyrolium-ui.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-tyrolium-ui',
-  imports: [],
-  template: ` <p>tyrolium-ui works!</p> `,
-  styles: ``,
-})
-export class TyroliumUi {}
--- projects/tyrolium-ui/src/public-api.ts
+++ projects/tyrolium-ui/src/public-api.ts
@@ -2,4 +2,4 @@
  * Public API Surface of tyrolium-ui
  */
 
-export * from './lib/tyrolium-ui';
+export * from './lib/components/tyro-ui-buttom/tyro-ui-buttom';
