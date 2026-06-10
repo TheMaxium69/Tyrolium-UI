4e30a4ed9d581929bc5ad025e276f8c4025f8c3a
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-11 00:42:53 +0200

    update

--- projects/tyrolium-ui/src/lib/services/tyro-ui-auth.service.ts
+++ projects/tyrolium-ui/src/lib/services/tyro-ui-auth.service.ts
@@ -9,7 +9,7 @@ import { TyroUiLangService } from './tyro-ui-lang.service';
  */
 export const TYRO_AUTH_API = new InjectionToken<string>('TYRO_AUTH_API', {
   providedIn: 'root',
-  factory: () => 'http://192.168.1.81/ApiUseritium',
+  factory: () => 'https://useritium.fr/api-externe',
 });
 
 const TOKEN_KEY    = 'tyrolium-token';
