e11e5496ced01701d75b7bef266253c9ed7fcb06
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-15 23:29:37 +0200

    update

--- projects/tyrolium-ui/src/lib/components/tyro-ui-not-found/tyro-ui-not-found.ts
+++ projects/tyrolium-ui/src/lib/components/tyro-ui-not-found/tyro-ui-not-found.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TyroUiLangService } from '../../services/tyro-ui-lang.service';
 
 @Component({
   selector: 'tyro-ui-not-found',
@@ -10,6 +11,9 @@ import { RouterLink } from '@angular/router';
 export class TyroUiNotFound {
   @Input() homeLink: string = '/';
   @Input() homeLabel: string = "Retour à l'accueil";
+  @Input() homeLabelEn: string = 'Back to home';
+
+  readonly lang = inject(TyroUiLangService).lang;
 
   goBack() {
     window.history.back();
