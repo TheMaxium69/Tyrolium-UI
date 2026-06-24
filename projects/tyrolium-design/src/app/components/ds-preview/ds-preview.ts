e9b15777634d00a6af204c88c1d90612d5bc99fc
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-25 01:06:25 +0200

    updadet

---
+++ projects/tyrolium-design/src/app/components/ds-preview/ds-preview.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ds-preview',
+  templateUrl: './ds-preview.html',
+  styleUrl: './ds-preview.css',
+})
+export class DsPreview {
+  @Input() label?: string;
+}
--- projects/tyrolium-design/src/app/pages/showcase/navbar/navbar-page.ts
+++ projects/tyrolium-design/src/app/pages/showcase/navbar/navbar-page.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { TyroUiNavbar } from 'tyrolium-ui';
+import { Component, inject } from '@angular/core';
+import { TyroUiNavbar, TyroUiPageHeader, TyroUiLangService } from 'tyrolium-ui';
+import { DsPreview } from '../../../components/ds-preview/ds-preview';
 
 @Component({
   selector: 'app-navbar-page',
-  imports: [TyroUiNavbar],
+  imports: [TyroUiNavbar, TyroUiPageHeader, DsPreview],
   templateUrl: './navbar-page.html',
   styleUrl: './navbar-page.css',
 })
-export class NavbarPage {}
+export class NavbarPage {
+  readonly lang = inject(TyroUiLangService).lang;
+}
