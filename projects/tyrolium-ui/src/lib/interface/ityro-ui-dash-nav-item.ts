ad1559493316364a6bf0d27911d2289f5bc121b8
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-24 20:46:10 +0200

    upodate

--- projects/tyrolium-ui/src/lib/interface/ityro-ui-dash-nav-item.ts
+++ projects/tyrolium-ui/src/lib/interface/ityro-ui-dash-nav-item.ts
@@ -1,12 +1,14 @@
 export interface ITyroUiDashNavChild {
   label: string;
   icon: string;
+  iconImg?: string;
   link: string;
 }
 
 export interface ITyroUiDashNavItem {
   label: string;
   icon: string;
+  iconImg?: string;
   link?: string;
   children?: ITyroUiDashNavChild[];
   open?: boolean;
